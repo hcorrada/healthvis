98be1625f5b49223dd907f5b7ab1ee89e458f72e
Jeff Leek <jtleek@gmail.com>
2012-11-12 16:44:22 -0500

    Added the listHealthVis function which lists all available healthvis plots

---
+++ R/listHealthVis.R
@@ -0,0 +1,8 @@
+#' This function lists all the currently available healthvis plots
+#'
+#' @export
+
+listHealthVis<- function(){
+  print("survivalVis() - Creates interactive survival plots")  
+  print("accuracyTableVis() - Creates interactive accuracy table plots")
+}
