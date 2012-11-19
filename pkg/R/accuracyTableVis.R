d0c27f12e62157bd8454c992097b7755633f5e2e
hcorrada <hcorrada@gmail.com>
2012-11-19 17:19:41 -0500

    switch to S4 complete

--- pkg/R/accuracyTableVis.R
+++ pkg/R/accuracyTableVis.R
@@ -6,35 +6,17 @@
 #' @param plot If TRUE the plot is launched in a browser. 
 #' @export
 #' 
-
 accuracyTableVis <- function(plot.title="Sensitivity/Specificity Plot",plot=TRUE,local=FALSE){
   # Create the healthvis object
-  healthvisObj <- list()
-  class(healthvisObj) <- "healthvis"
-  healthvisObj$type <- "accuracyTable"
-  healthvisObj$var.type <-c("continuous","continuous","continuous")
-  healthvisObj$var.list <- list(Sens=c(0,1),Spec=c(0,1),Prev=c(0,1))
-  healthvisObj$plot.title <- plot.title
+  healthvisObj = new("healthvis",
+                     plotType="accuracyTable",
+                     plotTitle=plot.title,
+                     varType=c("continuous","continuous","continuous"),
+                     varList=list(Sens=c(0,1),Spec=c(0,1),Prev=c(0,1)),
+                     local=local)
   
-  healthvisObj$url=ifelse(local, .localURL, .gaeURL)
-  url = sprintf("%s/post_data", healthvisObj$url)
-  cat("Posting data to URL:", url, "\n")
-  
-  obj.id = RCurl::postForm(sprintf("%s/post_data", healthvisObj$url),
-                           .params=list(
-                            plottitle=healthvisObj$plot.title,
-                            plottype=healthvisObj$type,
-                            varlist=rjson::toJSON(healthvisObj$var.list),
-                            vartype=rjson::toJSON(healthvisObj$var.type),
-                            varnames=rjson::toJSON(names(healthvisObj$var.list)))
-  )
-  if (obj.id == "error")
-    stop("Error posting data")
-  
-  healthvisObj$id=obj.id
-  
-  if(plot){
+  if (plot)
     plot(healthvisObj)
-  }
+  
   return(healthvisObj)
 }
--- pkg/R/healthvisMethods.R
+++ pkg/R/healthvisMethods.R
@@ -1,14 +1,48 @@
-#' A method for plotting a healthvis object
+#' Constructor for healthvis objects
+#' 
+#' Posts data to server
 #'
-#' @param x A healthvis object to be plotted
-#' @param file.title The file name for the HTML page to be created
-#' @S3method plot healthvis
-#' @method plot healthvis
-#' @return \code{NULL}
+#' @exportMethod initialize
+setMethod("initialize", signature=c(.Object="healthvis"),
+          function(.Object, plotType, plotTitle, varType, varList, d3Params=list(),local=FALSE) {
+            .Object@plotType=plotType
+            .Object@plotTitle=plotTitle
+            .Object@varType=varType
+            .Object@varList=varList
+            .Object@d3Params=d3Params
+            
+            .Object@url=ifelse(local, .localURL, .gaeURL)
+            postParams=list(
+              plottitle=plotTitle,
+              plottype=plotType,
+              varlist=rjson::toJSON(varList),
+              vartype=rjson::toJSON(varType),
+              varnames=rjson::toJSON(names(varList)))
+            
+            if (length(d3Params)>0)
+              postParams$d3Params=rjson::toJSON(d3Params)
+            
+            url = sprintf("%s/post_data", .Object@url)
+            cat("Posting data to URL:", url, "\n")
+            
+            serverID = RCurl::postForm(url,.params=postParams)
+            if(serverID=="error")
+              stop("Error posting data to server")
+            .Object@serverID=serverID
+            .Object
+})
 
-plot.healthvis <- function(x){
-  url=sprintf("%s/display/%s", x$url, x$id)
-  cat("Opening plot at URL: ", url, "\n")
-  browseURL(url)
-}
+if (!isGeneric("plot"))
+  setGeneric("plot", function(x,y,...) standardGeneric("plot"))
+
+#' Plot a healthvis object
+#' 
+#' @importMethodsFrom graphics plot
+#' @exportMethod plot
+setMethod("plot", signature=c("healthvis","missing"),
+          function(x,y,...) {
+            url=sprintf("%s/display/%s", x@url, x@serverID)
+            cat("Opening plot at URL: ", url, "\n")
+            browseURL(url)
+})
 
