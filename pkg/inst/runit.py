2e60c11438fb66920e340483db6e32d40a196edf
hcorrada <hcorrada@gmail.com>
2012-11-20 11:00:03 -0500

    using tornado makes local version really fast

--- pkg/inst/runit.py
+++ pkg/inst/runit.py
@@ -1,5 +1,12 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
 from application import app
 from application.settings import DEBUG
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
+    
