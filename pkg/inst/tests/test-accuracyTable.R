6c4f3a479d9e200f2fb8a071d81cb3f6db709d17
hcorrada <hcorrada@gmail.com>
2012-11-25 13:05:45 -0500

    finish cleaning up after package split

--- pkg/inst/tests/test-accuracyTable.R
+++
@@ -1,5 +0,0 @@
-test_that("accuracy table works on gae dev server", {
-  obj=accuracyTableVis(gae="local", plot=TRUE)
-  expect_that(obj@serverID!="error", is_true())
-})
-
--- pkg/inst/tests/test-survival.R
+++
@@ -1,6 +0,0 @@
-test_that("survival works on gae dev server", {
-  require(survival)
-  cobj=coxph(Surv(time, status) ~ trt+age+celltype,data=veteran)  
-  visCobj=survivalVis(cobj, veteran, plot.title="Veteran survival test", gae="local", plot=TRUE)
-  expect_that(visCobj@serverID!="error", is_true())
-})
