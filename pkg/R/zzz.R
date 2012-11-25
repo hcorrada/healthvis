b43ba351704724affc80d6a3bbbe050b354a550a
hcorrada <hcorrada@gmail.com>
2012-11-25 14:41:34 -0500

    memcache trick tested on gae

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,3 +1,3 @@
 .gaeLocalURL = "http://localhost:8080"
-.gaeURL = "http://healthviz.appspot.com"
+.gaeURL = "http://0-1-2.healthviz.appspot.com"
 .localURL = "http://127.0.0.1:5000"
--- pkg/inst/tests/test-local-accuracyTable.R
+++
@@ -1,4 +0,0 @@
-test_that("accuracy table works on local host", {
-  obj=accuracyTableVis(gae="none", plot=TRUE)
-  expect_that(obj@serverID!="error",is_true())
-})
