f30a50f4688a2f4610c0267b271052fc34ad0552
hcorrada <hcorrada@gmail.com>
2013-04-15 10:18:56 -0400

    localdata protocol tested on all plots

--- pkg/inst/tests/test-devel-paired.R
+++ pkg/inst/tests/test-devel-paired.R
@@ -1,7 +1,7 @@
 test_that("paired scatter works on gae devel server", {
   test_data <- iris
   test_data$content <- sample(c("High", "Med", "Low", "None"), nrow(test_data), replace=T)
-  pairedVis(test_data, plot.title="Iris Scatterplot")
+  #pairedVis(test_data, plot.title="Iris Scatterplot")
 
   obj=pairedVis(test_data, gaeDevel=TRUE, plot=TRUE)
   expect_that(obj@serverID!="error", is_true())
