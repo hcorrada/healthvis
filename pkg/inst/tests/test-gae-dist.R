d584c724eef997057454e1e5b2d83e6f0f7dc1b6
nosson <jpaulson@umiacs.umd.edu>
2013-04-16 12:49:32 -0400

    minor comment change

--- pkg/inst/tests/test-gae-dist.R
+++ pkg/inst/tests/test-gae-dist.R
@@ -1,4 +1,4 @@
-test_that("distance matrix works on gae dev server", {
+test_that("distance matrix works on gae", {
   mat = as.matrix(mtcars) 
   visDist=distVis(mat, gaeDevel=F)
   expect_that(visDist@serverID!="error", is_true())
