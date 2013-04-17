a8292b8d42aa8b38333ee58628d3c620018de710
hcorrada <hcorrada@gmail.com>
2013-04-16 20:46:41 -0400

    display after saving now retains form state

--- appengine/application/urls.py
+++ appengine/application/urls.py
@@ -11,13 +11,13 @@ app.add_url_rule('/', 'index', view_func=views.index)
 app.add_url_rule('/post_data', 'post_data', view_func=views.post_data, methods=['POST'])
 
 # display plot
-app.add_url_rule('/display/<string:id>', 'display', view_func=views.display)
+app.add_url_rule('/display/<string:plotid>', 'display', view_func=views.display)
 
 # display plot in embedabble format
-app.add_url_rule('/embed/<string:id>', 'embed', view_func=views.embed)
+app.add_url_rule('/embed/<string:plotid>', 'embed', view_func=views.embed)
 
 # save plot
-app.add_url_rule('/save/<string:id>', 'save', view_func=views.save)
+app.add_url_rule('/save/<string:plotid>', 'save', view_func=views.save)
 
 # finish saving plot
 app.add_url_rule('/upload/handler', 'finish_save', view_func=views.finish_save, methods=['POST'])
@@ -26,4 +26,4 @@ app.add_url_rule('/upload/handler', 'finish_save', view_func=views.finish_save, 
 app.add_url_rule('/tasks/remove_unsaved', 'remove_unsaved', view_func=views.remove_unsaved)
 
 # get d3params
-app.add_url_rule('/get_params/<string:id>', 'get_params', view_func=views.get_params)
+app.add_url_rule('/get_params/<string:plotid>', 'get_params', view_func=views.get_params)
