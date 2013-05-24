78b1e19ac5d72990aeca6a8c3689ab358b3fd195
hcorrada <hcorrada@gmail.com>
2013-05-23 22:54:54 -0400

    hotfix - version 1.2.1
    * Adds 'corVis' function
    * Adds row/column color labels to 'corVis' and 'distVis' functions
    * Sets the default value for continuous covariates to mid-point (this makes accuracyTable behave better)

--- appengine/application/forms.py
+++ appengine/application/forms.py
@@ -33,7 +33,7 @@ def generate_form(obj, requestargs=None):
         form_val = requestargs.get(name)
         if type == 'continuous':
             form_val = float(form_val) if form_val is not None else None
-            default = values[0] if form_val is None or form_val<values[0] or form_val>values[1] else form_val
+            default = (values[0] + values[1])/2.0 if form_val is None or form_val<values[0] or form_val>values[1] else form_val
             field = ContinuousCovariate(default=default, min=values[0], max=values[1])
         elif type == 'factor':
             default = values[0] if form_val is None or form_val not in values else form_val
